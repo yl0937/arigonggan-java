f6b3dfb6eb827c538cc422acee398647f3779095
yl0937 <yl1033371@gmail.com>
2023-10-17 09:26:44 +0900

    Feat : Initial Settings

---
+++ src/main/java/com/goorm/arigonggan/ArigongganApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.arigonggan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArigongganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArigongganApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/arigonggan/ArigongganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.arigonggan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArigongganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
